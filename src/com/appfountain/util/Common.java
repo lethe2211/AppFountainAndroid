bcdb386314d3660d8beebc35cad47ae072b3bac2
krrrr38 <k.kaizu38@gmail.com>
2013-10-12 18:29:25 +0900

    プログレスバー，ネットのチェックが出来るCommonクラス追加した

---
+++ src/com/appfountain/util/Common.java
@@ -0,0 +1,56 @@
+package com.appfountain.util;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * どこでも使えるCommonクラス
+ * プログレスバー，インターネットのチェック
+ */
+public class Common {
+	public static ProgressDialog progressBar;
+
+	/**
+	 * プログレスバーを表示する
+	 * 
+	 * @param parent
+	 * @param message
+	 */
+	public static void initializeProgressBar(Activity parent, String message) {
+		final ProgressDialog progressDialog = new ProgressDialog(parent);
+		progressDialog.setMessage(message);
+		progressDialog.setCancelable(false);
+		progressBar = progressDialog;
+		progressBar.show();
+	}
+
+	/**
+	 * プログレスバーを閉じる
+	 */
+	public static void closeProgressBar() {
+		if (progressBar != null && progressBar.isShowing()) {
+			progressBar.hide();
+		}
+	}
+
+	/**
+	 * インターネットが利用可能か調べる
+	 * @param context
+	 * @return
+	 */
+	public static Boolean isInternetAvailable(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (cm == null)
+			return false;
+
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni != null && ni.isAvailable() && ni.isConnected())
+			return true;
+		return false;
+
+	}
+}
