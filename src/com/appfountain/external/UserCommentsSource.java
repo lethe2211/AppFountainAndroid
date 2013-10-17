5094b5991e89170ecf0d893599ba16bbfa210458
Ken Kaizu <k.kaizu38@gmail.com>
2013-10-17 09:58:19 +0900

    ユーザの答えた質問・回答用のmodelとか作った

---
+++ src/com/appfountain/external/UserCommentsSource.java
@@ -0,0 +1,12 @@
+package com.appfountain.external;
+
+import com.appfountain.model.UserComments;
+
+public class UserCommentsSource extends BaseSource {
+	// for success case
+	private UserComments questions;
+
+	public UserComments getQuestions() {
+		return questions;
+	}
+}
