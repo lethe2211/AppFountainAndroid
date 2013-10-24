5326d29b3d579ce6b75228fe087e7698abd6ffdf
lethe <lethe2211@gmail.com>
2013-10-24 18:59:21 +0900

    NavigationDrawer リストのクリックイベント以外はできた

--- src/com/appfountain/TopPageActivity.java
+++ src/com/appfountain/TopPageActivity.java
@@ -8,12 +8,15 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -42,9 +45,9 @@ public class TopPageActivity extends ActionBarActivity {
 	private boolean isLast = false;
 	private List<Question> questions = new ArrayList<Question>();
 	private QuestionListAdapter questionListAdapter;
-	
+
 	private DrawerLayout drawerLayout; // DrawerLayout(NavigationDrawerを使うのに必要なレイアウト)
-    private ActionBarDrawerToggle drawerToggle; // ActionBar中のアイコンをタップすると，NavigationDrawerが開く/閉じるようにする
+	private ActionBarDrawerToggle drawerToggle; // ActionBar中のアイコンをタップすると，NavigationDrawerが開く/閉じるようにする
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,37 +59,58 @@ public class TopPageActivity extends ActionBarActivity {
 				R.layout.list_item_question, questions);
 		questionListView.setAdapter(questionListAdapter);
 		questionListView.setOnScrollListener(new EndlessScrollListener());
-		
+
 		// ActionBar中のアイコンのタップを有効にする
 		ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-        
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); // DrawerLayout
- 
-        // NavigationDrawerを開く/閉じるトグルボタン
-        drawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                R.drawable.ic_launcher,
-                R.string.drawer_open,
-                R.string.drawer_close
-        );
-        
-        // レイアウトにボタンを設定
-        drawerLayout.setDrawerListener(drawerToggle);
-        
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); // DrawerLayout
+
+		// NavigationDrawerを開く/閉じるトグルボタン
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_launcher, /* アイコン画像 */
+				R.string.drawer_open, /* Drawerを開くときのメッセージ(？) */
+				R.string.drawer_close /* Drawerを閉じるときのメッセージ(？) */
+		);
+
+		// レイアウトにボタンを設定
+		drawerLayout.setDrawerListener(drawerToggle);
+
+		// FIXME:なんでfinalいるの…？
+		final EditText searchEditText = (EditText) findViewById(R.id.search_text_box); // 検索ボックス
+		Button searchExecButton = (Button) findViewById(R.id.search_exec_btn); // NavigationDrawer中の検索ボタン
+
+		searchExecButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String query = searchEditText.getText().toString(); // 検索ボックスに入力されたクエリ
+				Log.d("search", query);
+
+			}
+		});
+
+		// ListViewとそれに対応するアダプタ
+		final ListView categoryListView = (ListView) findViewById(R.id.activity_top_page_category_list);
+		final ArrayAdapter<String> categoryListAdapter = new ArrayAdapter<String>(
+				this, R.layout.list_item_category); // list_item_category.xmlをレイアウトに指定(R.layout.simple_list_item_1と中身は同じ)
+		categoryListView.setAdapter(categoryListAdapter);
+
+		// 以下，リストに必要なカテゴリを加えていく
+		categoryListAdapter.add("windows");
+
 	}
-	
-	// よくわからないけど動く
+
+	// FIXME:よくわからないけど動く
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // ActionBar中のアプリアイコン(ホームボタン)がタップされたら
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// ActionBar中のアプリアイコン(ホームボタン)がタップされたら
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	protected void onResume() {
