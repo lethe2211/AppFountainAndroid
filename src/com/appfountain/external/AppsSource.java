3fc3894e9df89021bf9fe53927d95d3d8815ee6e
krrrr38 <k.kaizu38@gmail.com>
2013-11-01 09:54:12 +0900

    質問関連アプリ取得API用のクラス作成

---
+++ src/com/appfountain/external/AppsSource.java
@@ -0,0 +1,15 @@
+package com.appfountain.external;
+
+import java.util.List;
+
+import com.appfountain.model.App;
+
+/**
+ */
+public class AppsSource extends BaseSource {
+	private List<App> applications;
+
+	public List<App> getApplications() {
+		return applications;
+	}
+}
