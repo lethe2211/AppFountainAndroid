cf244115650a21f9c6b44fb537f28f65550b165d
lethe <lethe2211@gmail.com>
2013-11-09 09:12:50 +0900

    CommentSource.javaがaddされてなかったので追加

---
+++ src/com/appfountain/external/CommentSource.java
@@ -0,0 +1,15 @@
+package com.appfountain.external;
+
+import com.appfountain.model.Comment;
+
+/**
+ *  Commentを管理する
+ */
+public class CommentSource extends BaseSource {
+	private Comment comment;
+
+	public Comment getComment() {
+		return comment;
+	}
+
+}
